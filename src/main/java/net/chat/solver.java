9e2830824637a86ed58cbf25e65cc1da92a0e170
Koa <Koa@DESKTOP-1BECJMQ>
2022-12-29 04:09:34 +0000

    add quick

--- src/main/java/net/chat/solver.java
+++ src/main/java/net/chat/solver.java
@@ -11,19 +11,23 @@ public class Solver {
 
     public String testAll(){
         int maths = this.arithmetic();
-        if(maths != 0) {
+        if(maths != 65535) {
             return Integer.toString(maths);
+        }
+        String quick = this.quick();
+        if(quick != null){
+            return quick;
         }
         return null;
     }
 
     
     public int arithmetic() {
-        Pattern p = Pattern.compile("The first to solve '(\\d{1,4})\\s([-+])\\s(\\d{1,4})' wins!");
+        Pattern p = Pattern.compile("The first to solve '(\\d{1,4})\\s([-+x/])\\s(\\d{1,4})' wins!");
 		Matcher m = p.matcher(this.input);
         if(!m.find())
         {
-            return 0;
+            return 65535;
         }
 
         int x = Integer.parseInt(m.group(1));
@@ -34,6 +38,23 @@ public class Solver {
         if(op.equals("+")) {
             return x + y;
         }
+        else if(op.equals("x"))
+        {
+            return x * y;
+        }
+        else if(op.equals("/"))
+        {
+            return x/y;
+        }
         return x - y;
+    }
+    public String quick() {
+        Pattern p = Pattern.compile("The first to type '([\\d\\w]+)' wins!");
+        Matcher m = p.matcher(this.input);
+        if(!m.find())
+        {
+            return null;
+        }
+        return m.group(1);
     }
 }
