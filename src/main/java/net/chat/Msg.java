823f69181e5b1f7f52c5047ad47ba3779cd52977
Koa <Koa@DESKTOP-1BECJMQ>
2023-01-01 21:10:16 +0000

    faster!

--- src/main/java/net/chat/Msg.java
+++ src/main/java/net/chat/Msg.java
@@ -4,13 +4,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
 
 public class Msg {
-    final MinecraftClient client = MinecraftClient.getInstance();
+    private final MinecraftClient client = MinecraftClient.getInstance();
+	private ChatScreen cs = new ChatScreen("");
 
     public String send(String text) {
-		ChatScreen cs = new ChatScreen("");
-
 		this.client.setScreen(cs); // set and open chat box
-		cs.sendMessage(text, true); // Send message
+		this.cs.sendMessage(text, true); // Send message
 		this.client.setScreen(null); // Close screen
 		return text;
 	}
